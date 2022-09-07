c5a91b1fabd4353b9b513cecb05004b9c143d4dc
Alexandr <sashk599133@gmail.com>
2022-09-07 22:48:28 +0300

    Задание 7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 7: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int x = Convert.ToInt32(n.ToString()
+                            .AsEnumerable()
+                            .Last()
+                            .ToString());
+
+Console.WriteLine("Последняя цифра:" + x);
